7c9691bee2903772ec04ac7cbbbe56d64be55653
techneplus <jamesugbanu@gmail.com>
2019-07-18 10:15:43 +0100

    check for token validity

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,9 +1,10 @@
 import { check } from 'express-validator';
 import validation from '../middleware/validator';
 import TaskController from '../controller/TaskController';
+import userAuthenticate from '../middleware/userAuthenticate';
 
 const routes = (app) => {
-  app.post('/api/v1/task', [
+  app.post('/api/v1/task', userAuthenticate.authenticateUser, [
     check('taskName').not().isEmpty().withMessage('Task Name is required'),
     check('description').not().isEmpty().withMessage('Description is required'),
     check('assignee').not().isEmpty().withMessage('Assignee is required')
